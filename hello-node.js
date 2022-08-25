e9c8c8097ed86eda3eba3f191549b6c97a039d3f
Raney <graney1@una.edu>
2022-08-25 13:36:39 -0500

    updated express to include heroku port

--- hello-node.js
+++ hello-node.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('node:path')
 
 console.log("hello from node");
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Express')
+  //res.send('<h2>Hello World from Express</h2>')
+
+  res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
